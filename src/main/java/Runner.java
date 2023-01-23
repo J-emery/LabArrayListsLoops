6ce94943c56f298931d4ac5b1d0fdeaad1ba18aa
J-emery <jamzwj@gmail,com>
2023-01-23 16:12:24 +0000

    final commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,95 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+//        SCOTTISH ISLANDS
+        List<String> scottishIslands = new ArrayList<>();
+        scottishIslands.add("Jura");
+        scottishIslands.add("Mull");
+        scottishIslands.add("Skye");
+        scottishIslands.add("Arran");
+        scottishIslands.add("Tresco");
+
+//        1. Add "Coll" to the end of the list
+        scottishIslands.add("Coll");
+//        2. Add "Tiree" to the start of the list
+        scottishIslands.add(0,"Tiree");
+//        3. Add "Islay" after "Jura" and before "Mull"
+        scottishIslands.add(2,"Islay");
+//        4. Print out the index position of "Skye"
+        System.out.println(scottishIslands.indexOf("Skye"));
+//        5. Remove "Tresco" from the list by name
+        scottishIslands.remove("Tresco");
+//        6. Remove "Arran" from the list by index
+        scottishIslands.remove(5);
+//        7. Print the number of islands in your arraylist
+        System.out.println(scottishIslands.size());
+//        8. Sort the list alphabetically
+        Collections.sort(scottishIslands);
+//        9. Print out all the islands using a for loop
+        for(String island : scottishIslands){
+            System.out.println(island);
+        }
+
+        System.out.println(scottishIslands);
+
+//        NUMBERS
+        List<Integer> numbers = new ArrayList<>();
+        Collections.addAll(numbers, 1, 1, 4, 2, 7, 1, 6, 15, 13, 99, 7);
+
+        System.out.println("numbers: " + numbers);
+
+//        1. Print out a list of the even integers
+          for (Integer x : numbers ){
+             if (x%2==0){
+             System.out.println(x);
+             }
+          }
+//        2. Print the difference between the largest and smallest value
+          int Max = numbers.get(0);
+          int Min = numbers.get(0);
+          for(Integer x : numbers){
+              if (x > Max){
+              Max = x;
+              }
+              else if(x < Min){
+              Min = x;
+              }
+          } System.out.println(Max- Min);
+//        3. Print True if the list contains a 1 next to a 1 somewhere.
+        Boolean ones = false;
+        for (int i = 0 ; i < numbers.size()-1 ; i++){
+            if (numbers.get(i) == 1 && numbers.get(i+1) == 1){
+                ones = true;
+                break;
+            }
+        }
+        System.out.println(ones);
+//        4. Print the sum of the numbers,
+        int sum = 0;
+        for(int i : numbers){ //Q about reference variables/static (why each one?)
+            sum += i;
+        }
+        System.out.println(sum);
+//        5. Print the sum of the numbers...
+//           ...except the number 13 is unlucky, so it does not count...
+//           ...and numbers that come immediately after a 13 also do not count.
+//
+//          So [2, 7, 13, 2] would have sum of 9.
+        sum = 0;
+        for (int i=0; i<numbers.size();i++){
+            if (numbers.get(i)==13){
+                i++;
+            } else {
+                sum += numbers.get(i);
+            }
+        }
+        System.out.println(sum);
+
+    }
+
+}
